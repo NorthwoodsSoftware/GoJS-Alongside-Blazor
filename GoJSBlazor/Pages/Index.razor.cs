4753f8448e79b3b4943e6aeb08751c2b8ed284a5
Jon Hardy <jonathan.c.hardy@gmail.com>
2022-11-16 13:54:18 -0500

    .NET 6, GoJS 2.2

--- GoJSBlazor/Pages/Index.razor.cs
+++ GoJSBlazor/Pages/Index.razor.cs
@@ -1,31 +1,23 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using System;
 
-namespace GoJSBlazor.Pages
-{
-  public partial class GoJSMinimal : ComponentBase
-  {
+namespace GoJSBlazor.Pages {
+  public partial class Index : ComponentBase {
     [Inject] IJSRuntime JSRuntime { get; set; }
 
-    protected async override void OnAfterRender(bool firstRender)
-    {
-      if (firstRender)
-      {
+    protected async override void OnAfterRender(bool firstRender) {
+      if (firstRender) {
         // This calls the script in gojs-scripts.js
         await JSRuntime.InvokeAsync<string>("initGoJS");
       }
     }
 
-    protected async void Save()
-    {
+    protected async void Save() {
       await JSRuntime.InvokeAsync<string>("saveDiagram");
     }
 
-    protected async void Load()
-    {
+    protected async void Load() {
       await JSRuntime.InvokeAsync<string>("loadDiagram");
     }
-
   }
 }
